70f0defb69733177fe54d080c967bc7b641a235a
Yaoting Wang <ryanwang9966@gmail.com>
2022-03-16 03:17:51 +0000

    标注完 attention_layer

--- seq2seq/models/lstm.py
+++ seq2seq/models/lstm.py
@@ -121,7 +121,7 @@ class LSTMEncoder(Seq2SeqEncoder):
         # Embed tokens and apply dropout
         print(">>> forward():")
         # print("[test-01: src_tokens.size()]")
-        # print(src_tokens.size())  # torch.Size([10, 22])
+        # print(src_tokens.size())  # torch.Size([10, 22])  [本batch的句子数量, 单个句子的长度]
         batch_size, src_time_steps = src_tokens.size()  # -- 多少句子, 每个句子多少单词(time_step)
         src_embeddings = self.embedding(src_tokens)  # -- 获取根据src_tokens的词嵌入
         # print("[test-03: src_embeddings]")
@@ -217,6 +217,7 @@ class LSTMEncoder(Seq2SeqEncoder):
         2.  Describe what happens when self.bidirectional is set to True. 
             #-- If self.bidirectional is True, the final_hidden_states and final_cell_states of two directions will be 
             # concatenated separately by the dimension of hidden units
+            # 两个方向: the hidden units along the two directions are concatenated together.
             如果self.bidirectional为True，两个方向的final_hidden_states和final_cell_states会分别按照隐藏单元的维度拼接起来
         3.  What is the difference between final_hidden_states and final_cell_states?
             #-- The final_hidden_states is the last hidden unit of each sentence and the final_cell_states 
@@ -235,15 +236,16 @@ class LSTMEncoder(Seq2SeqEncoder):
             # print(f"cat-size: {final_hidden_states.size()}")  # torch.Size([1, 10, 128])
             # -- # [2, 10, 64] => [1, 10, 128], 把两个方向的hidden64拼接 [加入batch, 表示10句话用以填充一次hidden]
             final_cell_states = combine_directions(final_cell_states)
+            # [2, 10, 64] => [1, 10, 128]
             # --
         '''___QUESTION-1-DESCRIBE-A-END___'''
 
         # Generate mask zeroing-out padded positions in encoder inputs
-        src_mask = src_tokens.eq(self.dictionary.pad_idx)
+        src_mask = src_tokens.eq(self.dictionary.pad_idx)  # ele-wise 判断是否被mask. return tensor, masked的位置为True;
 
-        return {'src_embeddings': _src_embeddings.transpose(0, 1),
+        return {'src_embeddings': _src_embeddings.transpose(0, 1),  # [句子数(batch), 单词数(length), hidden_size]
                 'src_out': (lstm_output, final_hidden_states, final_cell_states),
-                'src_mask': src_mask if src_mask.any() else None}
+                'src_mask': src_mask if src_mask.any() else None}  # [batch_size, src_time_steps]
 
 
 class AttentionLayer(nn.Module):
@@ -253,18 +255,31 @@ class AttentionLayer(nn.Module):
         # Scoring method is 'general'
         self.src_projection = nn.Linear(input_dims, output_dims, bias=False)
         self.context_plus_hidden_projection = nn.Linear(input_dims + output_dims, output_dims, bias=False)
+        print("[test-09] attn-layer")
+        print("input-dims:", input_dims)    # 128
+        print("output-dims:", output_dims)  # 128
+        print("src_proj:", self.src_projection)  # Linear(in_features=128, out_features=128, bias=False)
+        print("con_+_hidden:", self.context_plus_hidden_projection)  # Linear(in_features=256, out_features=128, bias=False)
 
     def forward(self, tgt_input, encoder_out, src_mask):
         # tgt_input has shape = [batch_size, input_dims]
         # encoder_out has shape = [src_time_steps, batch_size, output_dims]
         # src_mask has shape = [batch_size, src_time_steps]
+        # todo: 这里input/output是啥? encoder_out是上面return的 (三维tuple吗?)
+        # todo: tgt_input是要输入的target句子吗?
 
         # Get attention scores
         # [batch_size, src_time_steps, output_dims]
+        # print('[test-09]')
+        # print("size-0::encoder_out:", encoder_out.size())  # torch.Size([22, 10, 128])
+        # print("value-0::encoder_out:", encoder_out)
         encoder_out = encoder_out.transpose(1, 0)  # -- encoder的输出张量 [句子数，单词数，隐藏单元数]
+        # print("size-1::encoder_out:", encoder_out.size())  # torch.Size([10, 22, 128])
+        # print("value-1::encoder_out:", encoder_out)
 
         # [batch_size, 1, src_time_steps]
         attn_scores = self.score(tgt_input, encoder_out)
+        # general_score: score(h(t), h(s)_) = h(t).T * Wa * h(s)_;
 
         '''
         ___QUESTION-1-DESCRIBE-B-START___
@@ -274,39 +289,95 @@ class AttentionLayer(nn.Module):
                 sent_tensor = create_sentence_tensor(...) 
                 # sent_tensor.size = [batch, sent_len, hidden]
         2.  Describe how the attention context vector is calculated. 
-         - First, we compute the attention score, by adding a new dimension to the masked token vector 'src_mask' 
-         and adapting masked_filled function to it to make all the False value equal to '-inf'. 
-         This way we can make the masked token account for nothing in the attention score matrix. 
-         - Then delete the dimension added just now and the score remains and we get attention weights. 
-         - At last, we multiple the attention weights and the output of the encoder to compute the attention 
-         context of the model.
+            - First, we compute the attention score, by adding a new dimension to the masked token vector 'src_mask' 
+              and adapting masked_filled function to it to make all the False value equal to '-inf'. 
+              This way we can make the masked token account for nothing in the attention score matrix. 
+            - Then delete the dimension added just now and the score remains and we get attention weights. 
+            - At last, we multiple the attention weights and the output of the encoder to compute the attention 
+              context of the model.
         3.  Why do we need to apply a mask to the attention scores?
-         - To make sure that the attention mechanism will not share any information of the tokens in the future, 
-         when we predict the token with previous ones.
+            - To make sure that the attention mechanism will not share any information of the tokens in the future, 
+              when we predict the token with previous ones.
+         # # # 是说把padded_0的零分attn转换为-inf吗? 
+         # 更详细的, 因为还要把attn_scores进行归一化, 有效的attn_score有正有负, 无效的attn_score=0; 
         '''
         if src_mask is not None:
-            src_mask = src_mask.unsqueeze(dim=1)
-            attn_scores.masked_fill_(src_mask, float('-inf'))
+            # print("[test-07]: unsqueeze")
+            # print("size-0::src-mask:", src_mask.size())     # torch.Size([10, 22])
+            # print("value-0::src-mask:", src_mask)
 
-        attn_weights = F.softmax(attn_scores, dim=-1)
-        attn_context = torch.bmm(attn_weights, encoder_out).squeeze(dim=1)
+            src_mask = src_mask.unsqueeze(dim=1)
+            # print("size-1::src-mask:", src_mask.size())     # torch.Size([10, 1, 22])  # 插入指定dim
+            # print("value-1::src-mask:", src_mask)
+
+            print("[test-08]: attn-score")
+            # print("size::attn_score:", attn_scores.size())  # torch.Size([10, 1, 22])
+            # print("value-0::attn_score:", attn_scores)
+            attn_scores.masked_fill_(src_mask, float('-inf'))    # todo: 设定成-inf. 其余的attn有负值, 因此使用-inf;
+            # print("value-1::attn_score:", attn_scores)         # 把在attn_scores中的value_0转化为value_-inf;
+
+        # 公式7: alignment_vec = align(h_t, h_s) = softmax(score(h_t, h_s));
+        # 所以, 这里的weights就是 alignment vector;
+        attn_weights = F.softmax(attn_scores, dim=-1)           # 将attn_scores归一化[0, 1];
+        # 使得每一句子(list[])的sum=1, 从而对每个word分配attn;  # 想象一下一个句子里面对不同的word有不同的attn权重(关注度);
+
+        # print("size::attn_weights:", attn_weights.size())     # torch.Size([10, 1, 22])
+        # print("value::attn_weights:", attn_weights)
+        attn_context = torch.bmm(attn_weights, encoder_out).squeeze(dim=1)  # [10,1,22], [10,22,128]) => [10,1,128] => [10,128]
+        # [√] todo: attn_context 表示什么?
+        # context (with attention) vector: c = attn_w * encoder_hidden.T,  # 对src_hidden的加权;
+        # 即: computed as the weighted average over all the source hidden states  [成立√]
         context_plus_hidden = torch.cat([tgt_input, attn_context], dim=1)
+        # todo: context_plus_hidden 表示什么?
+        # 公式(5):  h(t)~ = tanh(Wc[c(t); h(t)]  # 文中: 简单的把ct和ht的信息联合起来一起使用;
+        # Wc来自哪里? - 使用linear(), 会自动introduce这个weight;
         attn_out = torch.tanh(self.context_plus_hidden_projection(context_plus_hidden))
+        # todo: projection的作用是什么? 一个fc全连接层, 然后把"context_plus_hidden"的特征映射到这个dim=[x x]的空间?
+
+        # 步骤总结: p4, 3.1, 最后一段: go from ht -> at -> ct -> ht~;
         '''___QUESTION-1-DESCRIBE-B-END___'''
 
         return attn_out, attn_weights.squeeze(dim=1)
 
     def score(self, tgt_input, encoder_out):
-        """ Computes attention scores. """
+        """ Computes attention scores. """  # todo: 到底是 attention score 还是 alignment score?
 
         '''
         ___QUESTION-1-DESCRIBE-C-START___
         1.  Add tensor shape annotation to each of the output tensor
         2.  How are attention scores calculated? 
         3.  What role does batch matrix multiplication (i.e. torch.bmm()) play in aligning encoder and decoder representations?
+            - bmm([b, n, m], [b, m, p]) => [b, m, p]
+            - batch维度不变, [1, 22] * [22, 128] => [1, 128], 
         '''
+        # general_score: score(h(t), h(s)_) = h(t).T * Wa * h(s)_;
+        # 在哪里用了attention_weight Wa?
+        print("[test-10]: Proj in Score")
+        # print("size::encoder_out:", encoder_out.size())                            # torch.Size([10, 6, 128])
+        # print("value::encoder_out:", encoder_out)
+        # print("size::encoder_out_proj:", self.src_projection(encoder_out).size())  # torch.Size([10, 6, 128])
+        # print("value::encoder_out_prj:", self.src_projection(encoder_out))
         projected_encoder_out = self.src_projection(encoder_out).transpose(2, 1)
-        attn_scores = torch.bmm(tgt_input.unsqueeze(dim=1), projected_encoder_out)
+        # print("size::encoder_out_proj_tran:", projected_encoder_out.size())        # torch.Size([10, 128, 6])
+        # projection操作的作用:
+        # 扩增维度. 若有linear=Linear(20, 30), 有输入x=(128, 20), 则linear(x) = (128, 30)
+        # => linear(encoder_out) = (10, 22, 128) * (128, 128) => (10, 22, 128)  # 虽然size相同, 但是value不同;
+        # 22或者6, 都是句子长度(单词个数);
+        # y = x(W.T)  这里W.T是tensor_var, 属于内部变量;
+        # todo: 那么这里projected_encoder_out表示什么? 表示被[权重]处理过的encoder_output吗? (Wa * hs)
+        # todo: 疑问: Wa, 是指attention weight吗?, 实际上是从linear()函数中获取的, 不是我们算出来的吗?
+        # todo: 调用线性变换linear()的作用? 数值确实变了, 有什么作用?
+
+        attn_scores = torch.bmm(tgt_input.unsqueeze(dim=1), projected_encoder_out)  # todo: 这里用的不是简单的ht*hs吗?
+        # todo: 解答: 即: 不是简单的 ht*hs, 而是 ht * (Wa*hs), 即: general_score;
+        # todo: 但是一定是 ht * (Wa * hs)吗? 可以是 (ht * Wa) * hs 吗?
+        # todo: 这个Wa到底表示什么? attention weight? alignment weight? 但是在上面已经求出来attn_weight了.
+
+        # print("size::attn_score:", attn_scores.size())  # torch.Size([10, 1, 22])
+        # [10, 1, 128] * [10, 128, 22] = [10, 1, 22]
+        # 表示: 10个句子, 每个句子有一个score; ... 怎么解释? [22个浮点数], 有什么含义?
+        print("score:", attn_scores)
+
         '''___QUESTION-1-DESCRIBE-C-END___'''
 
         return attn_scores
